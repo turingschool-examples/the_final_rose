59ce397a9db296f344a4170e14f7faa8106d8537
Kim G <xiaole.guo@yahoo.com>
2022-04-22 10:10:29 -0600

    user story 2: contestant names are links to show page

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -27,6 +27,22 @@ RSpec.describe 'bachelorette contestants index page' do
 		expect(page).to_not have_content(contest4.hometown)
 	end
 
+	it 'has contestants names as links to their show page' do 
+
+		bach1 = Bachelorette.create!(name: 'Amy', season_number: 12, description: 'great season!')
+		bach2 = Bachelorette.create!(name: 'Susan', season_number: 13, description: 'another great season!')
+		contest1 = bach1.contestants.create!(name: 'Bob', age: 30, hometown: 'Denver')
+		contest2 = bach1.contestants.create!(name: 'Jim', age: 32, hometown: 'NYC')
+		contest3 = bach1.contestants.create!(name: 'Mike', age: 30, hometown: 'Hawaii')
+		contest4 = bach2.contestants.create!(name: 'Alex', age: 20, hometown: 'dont care')
+
+
+		visit bachelorette_contestants_path(bach1.id)
+
+		click_on 'Bob'
+		expect(current_path).to eq contestant_path(contest1.id)
+	end
+
 	it 'displays unique hometowns of contestants' do 
 		bach1 = Bachelorette.create!(name: 'Amy', season_number: 12, description: 'great season!')
 		contest1 = bach1.contestants.create!(name: 'Bob', age: 30, hometown: 'Denver')

60eae46de71b9dd0a4d8b6709973902fbc582513
Leo Banos Garcia <bsleo625@gmail.com>
2023-01-12 15:08:21 -0700

    Creates spec for bachelorette show page

---
+++ spec/features/bachelorettes/show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "Bachelorette Show page", type: :feature do
+  describe 'when visiting /bachelorettes/:id' do
+    it "see the bachelorette's name, season number, descriptiong of season they were on" do
+
+    end
+
+    it 'see a link to see the bachelorettes contestants'
+
+    it "when contestant link is clicked, user is taken to /bachelorettes/:bachelorette_id/contestants"
+
+    #build spec for /bach/bach_id/contestants
+  end
+end
